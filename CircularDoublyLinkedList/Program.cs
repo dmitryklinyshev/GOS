b9ad9be024be3864c87da0f443026ec80aa263bc
dmitryklinyshev <37501623+dmitryklinyshev@users.noreply.github.com>
2023-05-22 10:20:48 +0400

    Add files via upload

---
+++ CircularDoublyLinkedList/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CircularDoublyLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CircularDoublyLinkedList<string> circularList = new CircularDoublyLinkedList<string>();
+            circularList.Add("Tom");
+            circularList.Add("Bob");
+            circularList.Add("Alice");
+            circularList.Add("Sam");
+
+            foreach (var item in circularList)
+            {
+                Console.WriteLine(item);
+            }
+
+            circularList.Remove("Bob");
+            Console.WriteLine("\n После удаления: \n");
+            foreach (var item in circularList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ CircularLinkedList/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CircularLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CircularLinkedList<string> circularList = new CircularLinkedList<string>();
+
+            circularList.Add("Tom");
+            circularList.Add("Bob");
+            circularList.Add("Alice");
+            circularList.Add("Jack");
+            foreach (var item in circularList)
+            {
+                Console.WriteLine(item);
+            }
+
+            circularList.Remove("Bob");
+            Console.WriteLine("\n После удаления: \n");
+            foreach (var item in circularList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
